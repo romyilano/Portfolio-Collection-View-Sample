315db055c92466a1fad009049deb302521c9306c
Romy Ilano <romy@snowyla.com>
2015-07-21 18:17:18 -0700

    first

---
+++ SelfPortrait/SelfPortrait/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelfPortrait
+//
+//  Created by Romy on 7/21/15.
+//  Copyright © 2015 Romy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelfPortrait/SelfPortrait/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelfPortrait
+//
+//  Created by Romy on 7/21/15.
+//  Copyright © 2015 Romy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
